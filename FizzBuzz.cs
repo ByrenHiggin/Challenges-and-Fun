995ea16247de296d55d69557731d0ca5324feb4b
Byren Higgin <byrenhiggin1994@hotmail.com>
2017-08-01 08:50:34 +1000

    Create FizzBuzz.cs

---
+++ FizzBuzz.cs
@@ -0,0 +1,42 @@
+//This is technically a pseudocode attempt, I've never tried to compile this. It was created as an Over-the-Top response to the challenge.
+
+public static void main()
+{
+	const int FizzModuloValue = 3;
+	const int BuzzModuloValue = 3;
+
+	FizzBuzzConfigClass fbcc = new FizzBuzzConfigClass("Fizz",FizzModuloValue, "Buzz", BuzzModuloValue)
+
+	for(int i = 1; i < 101; i++)
+	{
+		FizzBuzz(i, fbcc);
+	}
+}
+
+public class FizzBuzzConfigClass()
+{
+	public string FirstModuloStringValue;
+	public string SecondModuloStringValue;
+	public int FirstModuloValue;
+	public int SecondModuloValue;
+	
+	public sub new(string FirstString, int FirstVal, string SecondString, int SecondVal) {
+		this.FirstModuloStringValue = FirstString;
+		this.SecondModuloStringValue = SecondString;
+		this.FirstModuloValue = FirstVal;
+		this.SecondModuloValue = SecondVal;
+	}
+
+}
+
+public static void FizzBuzz(int number, FizzBuzzConfigClass Conf) {
+	string s = "";
+	s = StringComparisonByModulo(Conf.FirstModuloStringValue, Conf.FirstModuloValue, number)
+	s = StringComparisonByModulo(Conf.SecondModuloStringValue, Conf.SecondModuloValue, number)
+	s = (s = "") ? s : number.toString();
+	console.WriteLine("s");
+}
+
+public static void StringComparisonByModulo(string output, int comparison, int test) {
+	return (test % number) == 0 ? output : "";
+}
